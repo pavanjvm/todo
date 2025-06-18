731302d1669dabfd976661e7f5ad7c9c58484906
pavanjvm <pavanprasad0506.com>
2025-06-18 14:01:39 +0530

    .

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {createtodo} = require('./types');
+const app = express();
+const port = 3000;}
+app = express();
+
+app.post('/todo', (req,res)=>{
+    const 
+
+})
+
+app.get('/todo', (req,res)=>{
+   
+})
